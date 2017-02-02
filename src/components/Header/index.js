6a3718e78e414cac7dcbf04d9526fc2f7057342c
Alejandro Oviedo <alexoviedo999@gmail.com>
2017-02-02 00:10:37 -0600

    styling updates

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,15 +21,17 @@ export default class Hero extends Component {
       <div id="particles-js" className="w-100 gradient h-650">
         <Nav />
         <div className="mw80 w-100 center relative eye-image">
-          <header className="w-100 tr">
-            <h1 className="home__title home__title--scaleconf">Rodrigo Belalcazar, MD PLLC</h1>
-            <h1 className="home__title home__title--colombia">Advanced Eye Center </h1>
-            <p className="home__date">
-              South Florida
-            </p>
-            <a className="btn btn--home" href="https://ti.to/colombia-dev/scaleconfco">
-              Contact Us
-            </a>
+          <header className="w-100 tr pa2 pa3-m pa4-l tc tr-m tr-l">
+            <div className="fr cf pa2 pa3-m pa4-l mr40 mr4-m mr4-l hero-wrap">
+              <h1 className="home__title home__title--scaleconf mt0">Rodrigo Belalcazar, MD PLLC</h1>
+              <h1 className="home__title home__title--colombia">Advanced Eye Center </h1>
+              <p className="home__date">
+                South Florida
+              </p>
+              <a className="btn btn--home" href="https://ti.to/colombia-dev/scaleconfco">
+                Contact Us
+              </a>
+            </div>
           </header>
         </div>
       </div>
